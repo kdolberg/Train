7f8d798a918a652f91fb2171eeafb243d490e7c6
krisdolberg <kristofor.d@gmail.com>
2024-01-06 12:38:27 -0800

    minor fixes

--- inc/argument_options.h
+++ inc/argument_options.h
@@ -1,14 +1,14 @@
 #ifndef ARGUMENT_OPTIONS_H
 #define ARGUMENT_OPTIONS_H
 
-#define INPUT_NET			'n'
-#define INPUT_TD			't'
-#define OUTPUT_NET			'o'
-#define LEARNING_RATE		'l'
+#define INPUT_NET				'n'
+#define INPUT_TD				't'
+#define OUTPUT_NET				'o'
+#define SET_LEARNING_RATE		'l'
 
-#define INPUT_NET_LONG		"innet"
-#define INPUT_TD_LONG		"td"
-#define OUTPUT_NET_LONG		"outnet"
-#define LEARNING_RATE_LONG  "learnrate"
+#define INPUT_NET_LONG			"innet"
+#define INPUT_TD_LONG			"td"
+#define OUTPUT_NET_LONG			"outnet"
+#define SET_LEARNING_RATE_LONG  "learnrate"
 
 #endif // ARGUMENT_OPTIONS_H
